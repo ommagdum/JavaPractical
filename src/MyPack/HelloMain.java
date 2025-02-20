a18ceac68937d204405db819ad6c9ccbf21b32ea
Om Magdum <om.college.id@gmail.com>
2025-02-20 11:47:57 +0530

    adding exp4 conducted in practicals

--- src/MyPack/HelloMain.java
+++ src/MyPack/HelloMain.java
@@ -1,8 +1,13 @@
 package MyPack;
+import SecPack.Second;
+import java.util.Scanner;
 
 public class HelloMain {
     public static void main(String[] args){
-        System.out.println("In MyPack package");
-        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+        Second s = new Second();
+        System.out.println("Enter A String : ");
+        String str = sc.nextLine();
+        s.display(str);
     }
 }
